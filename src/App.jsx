ad8d9c3354afcae1681ab47902acdce3870bb5e3
Temuujin undefined <23LP5268@G401-CC0096.local>
2024-01-08 14:25:47 +0800

    1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from "./components/Card";
+import Header from "./components/Header";
+export default function App() {
+  return (
+    <div>
+      <p>HelloWorld</p>
+      <Card />
+      <p></p>
+    </div>
+  );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+export default function Card() {
+  return (
+    <div>
+      <p>Hello World</p>
+      <img src alt="" />
+    </div>
+  );
+}
+// const Card = () => {
+//   return (
+//     <div>
+//       <p>Hello World</p>
+//     </div>
+//   );
+// };
+// export default Card;
